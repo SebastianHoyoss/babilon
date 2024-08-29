18f8ffff3c59159508cadd15812bfd08ed641206
SebastianHoyoss <141962219+SebastianHoyoss@users.noreply.github.com>
2024-08-28 20:50:44 -0500

    update infoApp class

--- programaBabilon.py
+++ programaBabilon.py
@@ -45,8 +45,27 @@ class Venta:
         self.pedido = pedido
         self.valor_total = valor_total
 
+class InfoApp:
+    def __init__(self,frame):
+        self.row_height=500
+        self.col_width=400
+        self.frame=frame
+        self.frameApp=Frame(self.frame,bg="#c3c3c3",width=550,height=400)
+        self.frameApp.grid(row=0,column=0,sticky="nsew")
+        Label(self.frameApp,text="Bienvenido a la aplicación de gestión administrativa de Babiloon Shoes", font=("Bold",12)).place(x=10,y=30)
+        descripcion="Mediante esta aplicación podrás gestionar activamente\n las ventas, clientes e inventario del almacén"
+        Label(self.frameApp,text=descripcion,font=("Bond",10)).place(x=10,y=150)
+
+def eliminar_frame():
+    for widget in root.winfo_children():
+        if(isinstance(widget,Frame)):
+            widget.destroy
 def info():
-    pass
+    eliminar_frame()
+    info_frame=tk.Frame(frame2,padx=5,pady=5,bg="sky blue")
+    InfoApp(info_frame)
+    info_frame.grid(row=1,column=0,sticky="nsew")
+    info_frame.pack_propagate(False)
 def func1():
     pass
 def func2():
@@ -66,11 +85,10 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.iconbitmap("babilon.ico.ico")
     root.title("Babiloon Shoes")
-    root.geometry("730x400")
+    root.geometry("750x500")
     root.resizable(False,False)
     frame1=tk.Frame(root,padx=10,pady=20,bd=1,relief="solid")
     frame2=tk.Frame(root,padx=10,pady=20,bd=1,relief="solid")
-    frameI=tk.Frame(frame2)
 
     frame1.grid(row=1, column=0, sticky="nsew")
     frame1.pack_propagate(False)
@@ -87,12 +105,12 @@ if __name__ == "__main__":
     boton_func4=tk.Button(frame1,text="Func4",command=func4)
     boton_func5=tk.Button(frame1,text="Func5",command=func5)
 
-    boton_info.grid(row=0,column=0,sticky="nsew")
-    boton_func1.grid(row=1,column=0,sticky="nsew")
-    boton_func2.grid(row=2,column=0,sticky="nsew")
-    boton_func3.grid(row=3,column=0,sticky="nsew")
-    boton_func4.grid(row=4,column=0,sticky="nsew")
-    boton_func5.grid(row=5,column=0,sticky="nsew")
+    boton_info.grid(row=0,column=0,padx=0,pady=10,sticky="nsew")
+    boton_func1.grid(row=1,column=0,padx=0,pady=10,sticky="nsew")
+    boton_func2.grid(row=2,column=0,padx=0,pady=10,sticky="nsew")
+    boton_func3.grid(row=3,column=0,padx=0,pady=10,sticky="nsew")
+    boton_func4.grid(row=4,column=0,padx=0,pady=10,sticky="nsew")
+    boton_func5.grid(row=5,column=0,padx=0,pady=10,sticky="nsew")
 
     frame1.configure(width=100,height=500,bg="azure",bd=5)
     frame2.configure(width=600,height=400,bg="light blue",bd=5)
